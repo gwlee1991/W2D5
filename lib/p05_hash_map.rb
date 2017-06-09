affbc031926498f6fba1e230e478c1daa87585b3
App Academy Student <student@appacademy.io>
2017-06-09 16:42:48 -0700

    completed linkedlist and hashmap

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -2,6 +2,7 @@ require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
   attr_reader :count
 
   def initialize(num_buckets = 8)
@@ -10,27 +11,65 @@ class HashMap
   end
 
   def include?(key)
+    mod = key.hash % num_buckets
+    @store[mod].each do |link|
+      if link.key == key
+        return true
+      end
+    end
+    false
   end
 
   def set(key, val)
+    mod = key.hash % num_buckets
+    if @store[mod].include?(key)
+      @store[mod].update(key,val)
+
+    else
+      @store[mod].append(key, val)
+      @count += 1
+    end
+    if @count > num_buckets
+      resize!
+    end
   end
 
   def get(key)
+    @store.each do |list|
+      return list.get(key) unless list.get(key).nil?
+    end
+    nil
   end
 
   def delete(key)
+    mod = key.hash % num_buckets
+    # p @store[mod]
+    @store[mod].each do |link|
+      if link.key == key
+        link.remove
+        break
+      end
+    end
+    @count -= 1
   end
 
-  def each
+  def each(&prc)
+
+    @store.each do |list|
+      list.each do |link|
+        prc.call(link.key, link.val)
+      end
+    end
+
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -42,6 +81,16 @@ class HashMap
   end
 
   def resize!
+    dupe = @store
+    @store = Array.new(num_buckets * 2) { LinkedList.new }
+    @count = 0
+    dupe.each do |bucket|
+      bucket.each do |link|
+        key = link.key
+        val = link.val
+        set(key, val)
+      end
+    end
   end
 
   def bucket(key)
